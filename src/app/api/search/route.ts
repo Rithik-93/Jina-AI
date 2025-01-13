25d2aec98b29f0c38d6bd41ef4d076f0905f3eb7
Rithik M <rithik7899@gmail.com>
2025-01-13 23:03:49 +0530

    prisma and product search api

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,33 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
 import { indexHost, pc } from "../actions/action";
 import { generateEmbedding } from "../actions/ai";
-// import { Pinecone } from '@pinecone-database/pinecone';
-
-// export const pc = new Pinecone({
-//   apiKey: process.env.PINECONE_API!,
-// });
-
+import prisma from "../../../../prisma/src";
 
 export async function POST(req: NextRequest) {
     const data = await req.json();
     const { query, clientId } = data;
     const namespace = clientId.toString();
     const embed = await generateEmbedding(query);
-    
-    const dat = await searchDB(embed, namespace);
-    return NextResponse.json({ dat })
+
+    const vectorIds = await searchVectorDB(embed, namespace);
+    const products = await searchDB(vectorIds)
+    return NextResponse.json({ products })
 }
-async function searchDB(queryVector: any, namespace: string) {
+
+async function searchVectorDB(queryVector: any, namespace: string) {
     try {
         const index = pc.index(namespace, indexHost);
         const queryResponse = await index.namespace(namespace).query({
             vector: queryVector,
-            topK: 4,
+            topK: 10,
             includeMetadata: true,
         });
-        return queryResponse.matches;
+        const vectorIds = queryResponse.matches.map(x => { return x.id })
+        return vectorIds;
     } catch (error) {
         console.error('Error querying Pinecone:', error);
         throw new Error(`Pinecone query failed: ${error}`);
     }
+}
+
+async function searchDB(vectorIds: string[]) {
+    const products = await prisma.item.findMany({
+        where: {
+            vectorId: {
+                in: vectorIds
+            }
+        }
+    })
+    return products
 }
